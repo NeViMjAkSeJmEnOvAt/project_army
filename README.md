e02fa4689e990b4cfe4a79a58eeb0a3414dd2c81
nevimjaksejmenovat <mikurecka@gmail.com>
2021-04-13 19:56:38 +0200

    0.3 - vytvoreni 3 modelu

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # project_army
 
-/// superuser ///
-name: michal
+</// superuser ///> 
+name: michal /
 password: michal
-////////////////
-
 
 verze 0.1 
 - vytvoření databáze v Django
 - přidání superuživatele
 
+verze 0.2
+- pridano startapp s nazvem Army_app
--- database/dzango/army/army_app/models.py
+++ database/dzango/army/army_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Solider(models.Model):
+    id = models.IntegerField(max_length=999999, unique=True, null=False)
+    name = models.CharField(max_length=50, help_text="Zadej jmeno vojaka.", null=False)
+    age = models.IntegerField(max_length=3, help_text="Zadej vek vojaka", null=False)
+    weapon = models.CharField(max_length=20, help_text="Zadej zbran vojaka", null=True)
+
+    def __str__(self):
+        return self.name
+
+class Gun(models.Model):
+    id = models.CharField(max_length=999999, unique=True, null=False)
+    name = models.CharField(max_length=50, help_text="Zadej jmeno zbrane.", null=False)
+    ammo = models.IntegerField(max_length=10, help_text="Zadej mnozstvi munice.", null=False)
+
+    def __str__(self):
+        return self.name
+
+class Platoon(models.Model):
+    id = models.CharField(max_length=999999, unique=True, null=False)
+    name = models.CharField(max_length=50, help_text="Zadej jmeno skupiny.", null=False)
+    leader = models.CharField(max_length=50, help_text="Zadej jmeno velitele skupiny.", null=False)
+
+    def __str__(self):
+        return self.name
